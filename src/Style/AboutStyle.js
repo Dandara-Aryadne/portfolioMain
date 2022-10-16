49c42bd3199b940c2de31e2cfc651b832fe8bc8e
Dandara-Aryadne <dandaraaryadne@hotmail.com>
2022-10-16 14:17:01 -0300

    esqueletando o About

--- src/Style/AboutStyle.js
+++ src/Style/AboutStyle.js
@@ -143,6 +143,7 @@ margin-right:18%;
 margin-top:5%;
 background-color: #F5F5F5;
 border:solid 1px black;
+
 @media screen and (min-width:360px) and (max-width:800px){
 display:flex;
 width:100%;
@@ -153,6 +154,106 @@ background-color: #F5F5F5;
 border:solid 1px black;
 
 }
+`
+
+export const Boximg = styled.div`
+display:flex;
+justify-content:center;
+align-items:center;
+position:absolute;
+border:solid;
+width:20%;
+height:40vh;
+
+`
+
+
+export const ImgFive=styled.img`
+display:flex;
+width:50%;
+height:40vh;
+right:73%;
+
+@media screen and (min-width:360px) and (max-width:800px){
+position:absolute;
+display:flex;
+width:24%;
+height:15vh;
+bottom:64%;
+left:37%;
+}
+
+`
+export const DivOne=styled.div`
+display:flex;
+justify-content:space-evenly;
+flex-direction:column;
+width: 28%;
+height:50%;
+margin-left:30%;
+margin-top:6%;
+text-align: justify;
+text-justify: inter-word;
+
+@media screen and (min-width:360px) and (max-width:800px){
+position:absolute;
+display:flex;
+justify-content:space-evenly;
+flex-direction:column;
+width: 88%;
+height:25%;
+margin-left:6%;
+margin-top:12%;
+text-align: justify;
+
+
+}
+`
+
+export const POne=styled.p`
+font-family: 'Exo', sans-serif;
+
+@media screen and (min-width:360px) and (max-width:800px){
+    font-size:10px;
+   
+}
+`
+
+export const FigureOne=styled.figure`
+position:absolute;
+
+left: 58%;
+bottom:14%;
+display:flex;
+align-items:center;
+justify-content:center;
+flex-wrap:wrap;
+width:40%;
+height:56vh;
+
+@media screen and (min-width:360px) and (max-width:800px){
+    position:absolute;
+    left:17%;
+    bottom:7%;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    flex-wrap:wrap;
+    width:65%;
+    height:30vh;
+
+}
+`
+export const ImgSix=styled.img`
+width:10%;
+display:flex;
+position:absolute;
+bottom:90%;
+right:80%;
+`
+export const ImgSeven =styled.img`
+width:35%;
+height:48%;
 `
 
 export const SectionTwo=styled.section`
@@ -174,5 +275,48 @@ border:solid 1px black;
 
 }
 `
+export const SectionThre=styled.section`
+display:flex;
+width:100%;
+height:60vh;
+margin-right:18%;
+margin-top:5%;
+background-color: #F5F5F5;
+border:solid 1px black;
+@media screen and (min-width:360px) and (max-width:800px){
+display:flex;
+width:100%;
+height:64vh;
+margin-right:18%;
+margin-top:25%;
+background-color: #F5F5F5;
+border:solid 1px black;
 
+}
+`
+export const SectionFour=styled.section`
+display:flex;
+width:100%;
+height:58vh;
+margin-right:18%;
+margin-top:5%;
+background-color: #F5F5F5;
+border:solid 1px black;
+@media screen and (min-width:360px) and (max-width:800px){
+display:flex;
+width:100%;
+height:64vh;
+margin-right:18%;
+margin-top:25%;
+background-color: #F5F5F5;
+border:solid 1px black;
+
+}
+`
+
+export const SectionFive = styled.section`
+width:100%;
+height:10vh;
+
+`
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Header from '../services/header/Header'
-
 import * as S from '../Style/AboutStyle'
 import Paint1 from '../imgAbout/Paint1.png'
 import Paint2 from '../imgAbout/Paint2.png'
@@ -8,6 +7,9 @@ import Paint3 from '../imgAbout/Paint3.png'
 import Paint4 from '../imgAbout/Paint4.png'
 import Profile1 from '../imgAbout/Profile1.png'
 import paint from '../imgAbout/paint.png'
+import element2 from '../imgAbout/element2.png'
+import Grid1 from '../imgAbout/Grid1.png'
+import Profile2 from '../imgAbout/Profile2.png'
 
 
 
@@ -31,12 +33,34 @@ export default function About() {
           <S.ImgFour src={Paint4} alt='paint4'/>
         </S.Figure>
       </S.Main>
-      <S.SectionOne>
 
+
+      <S.SectionOne>
+        <S.Boximg>
+        <S.ImgFive src={Profile2} alt='profile'/>
+        </S.Boximg>
+        <S.DivOne>
+          <S.POne>Me formei em Belas Artes, pela UFRRJ, um sonho realizado. Ser artista e viver da arte em suas diversas formas, me incentivou a sempre enxergar algo a mais no mundo. Foi durante a minha formação que eu descobri o tipo de pessoa que sou, quais são os meus objetivos e propósitos. O tempo que levei em formação valeram para me transformar no ser humano que sou hoje, assim como a minha arte é resultado disso. </S.POne>
+          <S.POne>Minha especialização é Ilustração, mas me formei com honras em História da Arte, com o tema do imaginário a visão feminina na arte.  Como o corpo feminino foi e é representando na arte até hoje. </S.POne>
+          <S.POne>Esse fator me incentivou dentro da arte a representar o feminino, dentro do realismo em tela. </S.POne>
+        </S.DivOne>
+        <S.FigureOne>
+          <S.ImgSix src={element2} alt='element'/>
+          <S.ImgSeven src={Grid1} alt='paint1'/>
+        </S.FigureOne>
+      
+      
       </S.SectionOne>
       <S.SectionTwo>
         
       </S.SectionTwo>
+      <S.SectionThre>
+        
+      </S.SectionThre>
+      <S.SectionFour>
+        
+      </S.SectionFour>
+      <S.SectionFive></S.SectionFive>
     </S.Body>
   )
 }
